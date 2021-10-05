117a9422518d5e17124398ade12d35290b3d70e0
TheodoreShelby <apeacefulmind2010@gmail.com>
2021-10-05 16:32:49 +0700

    clear redundant code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Main } from './components/unshared components/Main'
-import { Header } from './components/shared components/Header'
-import { Footer } from './components/shared components/Footer'
+import Main from './components/Main'
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
 
 
 const App = () => {
 	const [cart, setCart] = useState([])
 
-  return (<>
+  return (
 		<Router>
 			<Header cart={cart} />
 			<Main setCart={setCart} cart={cart} />
 			<Footer />
 		</Router>
-	</>)
+	)
 
 }
 
